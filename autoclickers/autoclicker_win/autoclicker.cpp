f3c75cb2ceeb7372123eaa03019e6b3e140d5fdd
Nickeron-dev <koshkin061969@gmail.com>
2021-09-27 23:44:17 +0300

    Started creating autoclicker of Windows using windows.h library.

---
+++ autoclickers/autoclicker_win/autoclicker.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <Windows.h>
+
+using std::cout;
+using std::endl;
+
+int main() 
+{
+	while (true) {
+		Sleep(50);
+		if (GetAsyncKeyState(VK_NUMPAD2) {
+			
+		}
+	}
+	return 0;
+}
+
